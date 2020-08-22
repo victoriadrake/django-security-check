e1994b6da31dd78535274da7eac94ac0972b55d5
Victoria Drake <hello@victoria.dev>
2020-08-22 10:23:02 -0400

    Add output file
    - Have the action create an output.txt file as output
    - Add the output 'result' to action.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 FAIL=${FAIL_LEVEL:=ERROR}
 MANAGE_PATH=${GITHUB_WORKSPACE}/${APP_PATH}
 REQS=${GITHUB_WORKSPACE}/${DEP_PATH}
@@ -13,15 +11,17 @@ if [[ "$ENV_TYPE" == "pipenv" ]]; then
     cd $REQS
     pip3 install pipenv
     PIPENV_IGNORE_VIRTUALENVS=1 pipenv install
-    cd $MANAGE_PATH && PIPENV_IGNORE_VIRTUALENVS=1 pipenv run python3 manage.py check --deploy --fail-level ${FAIL}
+    cd $MANAGE_PATH && PIPENV_IGNORE_VIRTUALENVS=1 pipenv run python3 manage.py check --deploy --fail-level ${FAIL} &> output.txt
 fi
 if [[ "$ENV_TYPE" == "venv" ]]; then
     pip install -r $REQS
-    cd $MANAGE_PATH && python manage.py check --deploy --fail-level ${FAIL}
+    cd $MANAGE_PATH && python manage.py check --deploy --fail-level ${FAIL} &> output.txt
 fi
 if [[ -z "$ENV_TYPE" ]]; then
     echo "No virtual environment specified."
     pip install django
-    cd $MANAGE_PATH && python manage.py check --deploy --fail-level ${FAIL}
+    cd $MANAGE_PATH && python manage.py check --deploy --fail-level ${FAIL} &> output.txt
 fi
 
+echo -e "\n--------- Django Security Check results ---------"
+cat output.txt
